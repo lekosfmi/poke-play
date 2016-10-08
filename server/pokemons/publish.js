d1d5087a8b4ccdc6c9bb71e2ae7de490d3eedd85
Vanielle Le <lephanvanielle@gmail.com>
2016-10-07 19:43:51 -0500

    ready to deploy

--- server/pokemons/publish.js
+++ server/pokemons/publish.js
@@ -2,6 +2,7 @@ import { Meteor } from 'meteor/meteor'
 import { Mongo } from 'meteor/mongo'
 import { Pokemons } from '../../collections/collections'
 
+
 Meteor.publish('pokemons', function pokemonsPublication() {
   return Pokemons.find()
 })
